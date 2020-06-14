9b1835a680fd4450a87f6318878e885ae1d24727
Joshua Scherschel <j.scherschel17@gmail.com>
2020-06-14 00:45:52 -0700

    testing moving content to shared dir

---
+++ shared/constants.go
@@ -0,0 +1,19 @@
+package shared
+
+import "github.com/google/uuid"
+
+type (
+	PostJson struct {
+		Title string `json:"title" binding:"required"`
+		Body string `json:"body" binding:"required"`
+		CreatedAt int64 `json:"createdAt"`
+		ID uuid.UUID `json:"id"`
+		Comments  map[string]CommentJson `json:"comments"`
+	}
+	CommentJson struct {
+		Body string `json:"body" binding:"required"`
+		CreatedAt int64 `json:"createdAt"`
+		ID string `json:"id"`
+	}
+	Posts map[uuid.UUID]PostJson
+)
